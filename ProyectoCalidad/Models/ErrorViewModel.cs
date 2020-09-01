6d1fce129ee66085076ac8703cf7c50e76cf41f2
fernandocajf <57675930+fernandocajf@users.noreply.github.com>
2020-09-01 17:23:04 -0500

    Se subieron todos los archivos de la carpeta

---
+++ ProyectoCalidad/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoCalidad.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
